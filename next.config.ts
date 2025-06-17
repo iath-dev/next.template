f71aeb4b93a663e9631bb26f2711e28eaf3098a8
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2025-06-17 14:24:14 -0500

    feat: configure image optimization settings in Next.js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    minimumCacheTTL: 60 * 60 * 24,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 258, 384],
+  },
 };
 
 export default nextConfig;
